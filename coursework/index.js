f2c70227681f521bf9346ed778d3bda38527f874
gyonungi <104500843+gyonungi@users.noreply.github.com>
2022-09-27 17:21:52 +0700

    Update index.js

--- coursework/index.js
+++ coursework/index.js
@@ -11,16 +11,12 @@ startButton.addEventListener('click',function(){
     let checkedRadio = document.querySelector('input[type="radio"]:checked');
     console.log(checkedRadio);
 		if (checkedRadio) {
-            window.gameState.leve = checkedRadio.level;
+            window.gameState.level = checkedRadio.value;
         window.location.href = "/game.html"
     }else{
-        console.log('Выберите уровень сложности');
+        alert('Выберите уровень сложности');
     }
-     checkedRadio = document.querySelector('input[type="radio"]:checked').value;
+    
     })
 
-    window.gameState = {
-        level: 1,
-        level: 2,
-        level: 3
-    }
+  
